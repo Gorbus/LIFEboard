ed189d08af163eff65f4ebea80d488a8b3c8505c
ivonig <ivonig.corfmat@gmail.com>
2017-12-24 07:13:16 +0100

    add donator name

--- imports/ui/Footer.js
+++ imports/ui/Footer.js
@@ -5,6 +5,7 @@ export default () => {
 		<div className="footer">
 			<div>LIFEboard.me has been developped by <a className="44dev" href="http://www.44dev.com" target='_blank'>44dev</a>. It is a community supported project and has no legal boundary with the official LIFE project or website.</div>
 			<div>If you like LIFEboard.me, donations are much appreciated : 0xDF8B01C227b411D28aA9aC5af492E7f20fD2bf63 (preferably LIFE but ETH or any ERC20 token are welcome)</div>
+			<div>Thanks to those who already made a donation : Linkdefeu</div>
 		</div>
 	)
 }
